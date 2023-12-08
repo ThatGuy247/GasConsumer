10adf781a7bfbe0e1845a54573a451cb9d9b7a8d
Arman Emamhosseini <75748147+ThatGuy247@users.noreply.github.com>
2023-12-09 00:47:24 +0330

    Create GasConsumer.sol
    Add Functions

---
+++ GasConsumer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract GasConsumer {
+    uint256[] public data;
+
+    // Function that consumes gas by performing a loop
+    function consumeGas(uint256 iterations) external {
+        for (uint256 i = 0; i < iterations; i++) {
+            data.push(i);
+        }
+    }
+}
