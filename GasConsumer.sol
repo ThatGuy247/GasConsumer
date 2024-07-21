d6b04b7f58d01bd507ece53bbf5a96a349af2426
Arman Emamhosseini <75748147+ThatGuy247@users.noreply.github.com>
2024-07-21 13:00:06 +0330

    Update GasConsumer.sol

--- GasConsumer.sol
+++ GasConsumer.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 contract GasConsume {
     uint256[] public data;
   
-    // Function that consumes gas by performing a loop  
+    // Function that consumes gas by performing a loop   
     function consumeGas(uint256 iterations) external {
         for (uint256 i = 0; i < iterations; i++) {
             data.push(i);
